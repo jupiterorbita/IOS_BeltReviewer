df1c1f2880c87219430e2885da675ea80b6a8367
jupterorbita <john.williams@outlook.com>
2018-07-19 11:03:30 -0700

    removed keyboard dismiss bc simulator keyboard

--- beltReview/AddEditVC.swift
+++ beltReview/AddEditVC.swift
@@ -61,11 +61,6 @@ class AddEditVC: UIViewController {
 //        performSegue(withIdentifier: "unwindsegueFromAddEditVC", sender: self)
     }
     
-    
-    //keyboard dismiss outside
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
